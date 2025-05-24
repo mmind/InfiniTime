mmind/InfiniTime

commit 63e0c4f4efb047f4f15601f7b08640c4b589de93

Application selection at build time
A list of "user applications" is built at compile time. It contains all the info needed to create the application at runtime (ptr to a create() function) and to display the app in the application menu. All applications declare a TypeTrait with these information.
When a new app must be loaded, DisplayApp first check if this app is a System app (in which case it creates it like it did before). If it's not a System app, it looks for the app in the list of User applications and creates it if it found it.
Those changes allow to more easily add new app and to select which app must be built into the firmware.
Switch to C++20 (and fix a few issues in SpiMaster.cpp and Watchdog.cpp.

src/displayapp/DisplayApp.cpp
@@ -50,6 +50,7 @@
 #include "displayapp/screens/settings/SettingBluetooth.h"
 
 #include "libs/lv_conf.h"
+#include "UserApps.h"
 
 using namespace Pinetime::Applications;
 using namespace Pinetime::Applications::Display;
@@ -96,7 +97,12 @@ DisplayApp::DisplayApp(Drivers::St7789& lcd,
     touchHandler {touchHandler},
     filesystem {filesystem},
     lvgl {lcd, filesystem},
-    timer(this, TimerCallback) {
+    timer(this, TimerCallback),
+    controllers{
+      batteryController, bleController, dateTimeController, notificationManager, heartRateController,
+      settingsController, motorController, motionController, alarmController, brightnessController,
+      nullptr, filesystem, timer, nullptr, this, lvgl, nullptr, nullptr}
+  {
 }
 
 void DisplayApp::Start(System::BootErrors error) {
@@ -402,14 +408,21 @@ void DisplayApp::LoadScreen(Apps app, DisplayApp::FullRefreshDirections directio
   SetFullRefresh(direction);
 
   switch (app) {
-    case Apps::Launcher:
-      currentScreen =
-        std::make_unique<Screens::ApplicationList>(this, settingsController, batteryController, bleController, dateTimeController, filesystem);
-      break;
-    case Apps::Motion:
-      // currentScreen = std::make_unique<Screens::Motion>(motionController);
-      // break;
-    case Apps::None:
+    case Apps::Launcher: {
+      std::array<Screens::Tile::Applications, UserAppTypes::Count> apps;
+      int i = 0;
+      for (const auto& userApp : userApps) {
+        apps[i++] = Screens::Tile::Applications {userApp.icon, userApp.app, true};
+      }
+      currentScreen = std::make_unique<Screens::ApplicationList>(this,
+                                                                 settingsController,
+                                                                 batteryController,
+                                                                 bleController,
+                                                                 dateTimeController,
+                                                                 filesystem,
+                                                                 std::move(apps));
+    }
+    break;
     case Apps::Clock:
       currentScreen = std::make_unique<Screens::Clock>(dateTimeController,
                                                        batteryController,
@@ -421,7 +434,6 @@ void DisplayApp::LoadScreen(Apps app, DisplayApp::FullRefreshDirections directio
                                                        systemTask->nimble().weather(),
                                                        filesystem);
       break;
-
     case Apps::Error:
       currentScreen = std::make_unique<Screens::Error>(bootError);
       break;
@@ -453,14 +465,6 @@ void DisplayApp::LoadScreen(Apps app, DisplayApp::FullRefreshDirections directio
                                                                *systemTask,
                                                                Screens::Notifications::Modes::Preview);
       break;
-    case Apps::Timer:
-      currentScreen = std::make_unique<Screens::Timer>(timer);
-      break;
-    case Apps::Alarm:
-      currentScreen = std::make_unique<Screens::Alarm>(alarmController, settingsController.GetClockType(), *systemTask, motorController);
-      break;
-
-    // Settings
     case Apps::QuickSettings:
       currentScreen = std::make_unique<Screens::QuickSettings>(this,
                                                                batteryController,
@@ -516,38 +520,25 @@ void DisplayApp::LoadScreen(Apps app, DisplayApp::FullRefreshDirections directio
     case Apps::FlashLight:
       currentScreen = std::make_unique<Screens::FlashLight>(*systemTask, brightnessController);
       break;
-    case Apps::StopWatch:
-      currentScreen = std::make_unique<Screens::StopWatch>(*systemTask);
+    default: {
+      const auto* d = std::find_if(userApps.begin(), userApps.end(), [app](const AppDescription& appDescription) {
+        return appDescription.app == app;
+      });
+      if (d != userApps.end())
+        currentScreen.reset(d->create(controllers));
+      else {
+        currentScreen = std::make_unique<Screens::Clock>(dateTimeController,
+                                                         batteryController,
+                                                         bleController,
+                                                         notificationManager,
+                                                         settingsController,
+                                                         heartRateController,
+                                                         motionController,
+                                                         systemTask->nimble().weather(),
+                                                         filesystem);
+      }
       break;
-    case Apps::Twos:
-      currentScreen = std::make_unique<Screens::Twos>();
-      break;
-    case Apps::Paint:
-      currentScreen = std::make_unique<Screens::InfiniPaint>(lvgl, motorController);
-      break;
-    case Apps::Paddle:
-      currentScreen = std::make_unique<Screens::Paddle>(lvgl);
-      break;
-    case Apps::Music:
-      currentScreen = std::make_unique<Screens::Music>(systemTask->nimble().music());
-      break;
-    case Apps::Navigation:
-      currentScreen = std::make_unique<Screens::Navigation>(systemTask->nimble().navigation());
-      break;
-    case Apps::HeartRate:
-      currentScreen = std::make_unique<Screens::HeartRate>(heartRateController, *systemTask);
-      break;
-    case Apps::Metronome:
-      currentScreen = std::make_unique<Screens::Metronome>(motorController, *systemTask);
-      break;
-    /* Weather debug app
-    case Apps::Weather:
-      currentScreen = std::make_unique<Screens::Weather>(this, systemTask->nimble().weather());
-      break;
-    */
-    case Apps::Steps:
-      currentScreen = std::make_unique<Screens::Steps>(motionController, settingsController);
-      break;
+    }
   }
   currentApp = app;
 }
@@ -605,6 +596,19 @@ void DisplayApp::PushMessageToSystemTask(Pinetime::System::Messages message) {
 
 void DisplayApp::Register(Pinetime::System::SystemTask* systemTask) {
   this->systemTask = systemTask;
+  this->controllers.systemTask = systemTask;
+}
+
+void DisplayApp::Register(Pinetime::Controllers::WeatherService* weatherService) {
+  this->controllers.weatherController = weatherService;
+}
+
+void DisplayApp::Register(Pinetime::Controllers::MusicService* musicService) {
+  this->controllers.musicService = musicService;
+}
+
+void DisplayApp::Register(Pinetime::Controllers::NavigationService* NavigationService) {
+  this->controllers.navigationService = NavigationService;
 }
 
 void DisplayApp::ApplyBrightness() {

CMakeLists.txt
@@ -5,7 +5,7 @@ set(CMAKE_BUILD_TYPE Release CACHE STRING "Choose Debug or Release")
 project(pinetime VERSION 1.13.0 LANGUAGES C CXX ASM)
 
 set(CMAKE_C_STANDARD 99)
-set(CMAKE_CXX_STANDARD 14)
+set(CMAKE_CXX_STANDARD 20)
 
 # set(CMAKE_GENERATOR "Unix Makefiles")
 set(CMAKE_C_EXTENSIONS OFF)

src/CMakeLists.txt
@@ -394,7 +394,6 @@ list(APPEND SOURCE_FILES
         displayapp/screens/Notifications.cpp
         displayapp/screens/Twos.cpp
         displayapp/screens/HeartRate.cpp
-        displayapp/screens/Motion.cpp
         displayapp/screens/FlashLight.cpp
         displayapp/screens/List.cpp
         displayapp/screens/CheckboxList.cpp

src/displayapp/Apps.h
@@ -1,5 +1,5 @@
 #pragma once
-
+#include <cstddef>
 namespace Pinetime {
   namespace Applications {
     enum class Apps {
@@ -37,7 +37,32 @@ namespace Pinetime {
       SettingChimes,
       SettingShakeThreshold,
       SettingBluetooth,
-      Error
+      Error,
+      Weather
     };
+    template <Apps>
+    struct AppTraits {};
+
+    template<Apps ...As>
+    struct TypeList {
+      static constexpr size_t Count = sizeof...(As);
+    };
+
+    using UserAppTypes = TypeList<Apps::Alarm,
+                                  Apps::HeartRate,
+                                  Apps::Paint,
+                                  Apps::Metronome,
+                                  Apps::Music,
+                                  Apps::Navigation,
+                                  Apps::Paddle,
+                                  Apps::Steps,
+                                  Apps::StopWatch,
+                                  Apps::Timer,
+                                  Apps::Twos
+                                  /*
+                                  Apps::Weather,
+                                  Apps::Motion
+                                  */
+                                  >;
   }
 }

src/displayapp/Controllers.h
@@ -1,0 +1,51 @@
+#pragma once
+namespace Pinetime {
+  namespace Applications {
+    class DisplayApp;
+  }
+  namespace Components {
+    class LittleVgl;
+  }
+  namespace Controllers {
+    class Battery;
+    class Ble;
+    class DateTime;
+    class NotificationManager;
+    class HeartRateController;
+    class Settings;
+    class MotorController;
+    class MotionController;
+    class AlarmController;
+    class BrightnessController;
+    class WeatherService;
+    class FS;
+    class Timer;
+    class MusicService;
+    class NavigationService;
+  }
+  namespace System {
+    class SystemTask;
+  }
+  namespace Applications {
+    struct AppControllers {
+      const Pinetime::Controllers::Battery& batteryController;
+      const Pinetime::Controllers::Ble& bleController;
+      Pinetime::Controllers::DateTime& dateTimeController;
+      Pinetime::Controllers::NotificationManager& notificationManager;
+      Pinetime::Controllers::HeartRateController& heartRateController;
+      Pinetime::Controllers::Settings& settingsController;
+      Pinetime::Controllers::MotorController& motorController;
+      Pinetime::Controllers::MotionController& motionController;
+      Pinetime::Controllers::AlarmController& alarmController;
+      Pinetime::Controllers::BrightnessController& brightnessController;
+      Pinetime::Controllers::WeatherService* weatherController;
+      Pinetime::Controllers::FS& filesystem;
+      Pinetime::Controllers::Timer& timer;
+      Pinetime::System::SystemTask* systemTask;
+      Pinetime::Applications::DisplayApp* displayApp;
+      Pinetime::Components::LittleVgl& lvgl;
+      Pinetime::Controllers::MusicService* musicService;
+      Pinetime::Controllers::NavigationService* navigationService;
+    };
+  }
+}

src/displayapp/DisplayApp.h
@@ -20,6 +20,7 @@
 #include "BootErrors.h"
 
 #include "utility/StaticStack.h"
+#include "displayapp/Controllers.h"
 
 namespace Pinetime {
 
@@ -73,6 +74,9 @@ namespace Pinetime {
       void SetFullRefresh(FullRefreshDirections direction);
 
       void Register(Pinetime::System::SystemTask* systemTask);
+      void Register(Pinetime::Controllers::WeatherService* weatherService);
+      void Register(Pinetime::Controllers::MusicService* musicService);
+      void Register(Pinetime::Controllers::NavigationService* NavigationService);
 
     private:
       Pinetime::Drivers::St7789& lcd;
@@ -96,6 +100,7 @@ namespace Pinetime {
       Pinetime::Components::LittleVgl lvgl;
       Pinetime::Controllers::Timer timer;
 
+      AppControllers controllers;
       TaskHandle_t taskHandle;
 
       States state = States::Running;

src/displayapp/UserApps.h
@@ -1,0 +1,36 @@
+#pragma once
+#include "Apps.h"
+#include "Controllers.h"
+
+#include "displayapp/screens/Alarm.h"
+#include "displayapp/screens/Timer.h"
+#include "displayapp/screens/Twos.h"
+#include "displayapp/screens/Tile.h"
+#include "displayapp/screens/ApplicationList.h"
+#include "displayapp/screens/Clock.h"
+
+namespace Pinetime {
+  namespace Applications {
+    namespace Screens {
+      class Screen;
+    }
+
+    struct AppDescription {
+      Apps app;
+      const char* icon;
+      Screens::Screen* (*create)(AppControllers& controllers);
+    };
+
+    template <Apps t>
+    consteval AppDescription CreateAppDescription() {
+      return {AppTraits<t>::app, AppTraits<t>::icon, &AppTraits<t>::Create};
+    }
+
+    template <template<Apps...> typename T, Apps ...ts>
+    consteval std::array<AppDescription, sizeof...(ts)> CreateAppDescriptions(T<ts...>) {
+      return {CreateAppDescription<ts>()...};
+    }
+
+    constexpr auto userApps = CreateAppDescriptions(UserAppTypes {});
+  }
+}

src/displayapp/screens/Alarm.cpp
@@ -19,6 +19,10 @@
 #include "displayapp/screens/Screen.h"
 #include "displayapp/screens/Symbols.h"
 #include "displayapp/InfiniTimeTheme.h"
+#include "components/settings/Settings.h"
+#include "components/alarm/AlarmController.h"
+#include "components/motor/MotorController.h"
+#include "systemtask/SystemTask.h"
 
 using namespace Pinetime::Applications::Screens;
 using Pinetime::Controllers::AlarmController;

commit a6ad549bacd6c577b8f7b7a5d313a5265028551c

navigation: fix greying out the app icon if not enabled
Commit 0aead42fdf51 ("navigation: Add is available (#1847)") added the
ability to draw the app icon in grey and in a disabled state when some
prerequisits were not met. Only the Navigation app was using this mechanism
due to its icons being stored in the external memory and possibly missing.

Commit 63e0c4f4efb0 ("Application selection at build time") broke this by
always setting the state as true:
      for (const auto& userApp : userApps) {
        apps[i++] = Screens::Tile::Applications {userApp.icon, userApp.app, true};
      }

As right now only the Navigation app is using this, fix the issue by just
checking the already existing Navigation's IsAvailable method when its icon
is getting added.

src/displayapp/DisplayApp.cpp
@@ -518,7 +518,13 @@ void DisplayApp::LoadScreen(Apps app, DisplayApp::FullRefreshDirections directio
       std::array<Screens::Tile::Applications, UserAppTypes::Count> apps;
       int i = 0;
       for (const auto& userApp : userApps) {
-        apps[i++] = Screens::Tile::Applications {userApp.icon, userApp.app, true};
+        bool enabled = true;
+
+        // Right now, only the Navigation app could be missing files
+        if (userApp.app == Apps::Navigation)
+            enabled = Applications::Screens::Navigation::IsAvailable(filesystem);
+
+        apps[i++] = Screens::Tile::Applications {userApp.icon, userApp.app, enabled};
       }
       currentScreen = std::make_unique<Screens::ApplicationList>(this,
                                                                  settingsController,